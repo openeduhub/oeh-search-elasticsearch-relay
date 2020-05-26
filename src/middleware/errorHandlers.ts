4a7d52e7c88d41eca43acb75fbdbbaa2a43c8827
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-05-26 09:58:20 +0200

    improve error logging

--- src/middleware/errorHandlers.ts
+++ src/middleware/errorHandlers.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import { config } from '../config';
 import { ErrorResponse } from '../types/ErrorResponse';
+import { GraphQLError } from 'graphql';
+
+function logError(...params: any[]) {
+    console.error(new Date().toUTCString(), ...params);
+}
 
 function wrapRestError(router: Router) {
     router.use('/rest', (err: Error, req: Request, res: Response, next: NextFunction) => {
-        console.error(err);
+        logError(`${err.name} on ${req.path}: ${err.message}`);
         const message: ErrorResponse = {
             error: err.name,
             message: err.message,
@@ -16,6 +21,10 @@ function wrapRestError(router: Router) {
         const status = (err as any).status || 500;
         res.status(status).send(message);
     });
+}
+
+export function logGraphQlError(error: GraphQLError) {
+    logError(`${error.name} on ${error.path}: ${error.message}`);
 }
 
 export default [wrapRestError];
--- src/middleware/graphql.ts
+++ src/middleware/graphql.ts
@@ -3,9 +3,17 @@ import { Express, Router } from 'express';
 import { importSchema } from 'graphql-import';
 import * as path from 'path';
 import resolvers from '../resolvers';
+import { GraphQLError } from 'graphql';
+import { logGraphQlError } from './errorHandlers';
 
 const typeDefs = importSchema(path.join(__dirname, '..', 'schema.graphql'));
-const server = new ApolloServer({ typeDefs, resolvers });
+
+function formatError(error: GraphQLError): GraphQLError {
+    logGraphQlError(error);
+    return error;
+}
+
+const server = new ApolloServer({ typeDefs, resolvers, formatError });
 
 export const handleGraphQl = (router: Router) =>
     server.applyMiddleware({ app: router as Express, path: '/graphql' });

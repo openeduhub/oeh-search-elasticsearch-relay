b10239a67e513463bfc47c0883374d2d66f6827d
Christopher Lübbemeier <luebbemeier@metaventis.com>
2021-02-08 17:00:50 +0100

    feat: switch from graphql-import to -tools

--- src/server/middleware/graphql.ts
+++ src/server/middleware/graphql.ts
@@ -1,21 +1,29 @@
 import { ApolloServer } from 'apollo-server-express';
 import { Express, Router } from 'express';
-import { importSchema } from 'graphql-import';
 import * as path from 'path';
 import resolvers from '../../graphql/resolvers';
 import { GraphQLError } from 'graphql';
 import { logGraphQlError } from './errorHandlers';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { addResolversToSchema } from '@graphql-tools/schema';
 
-const typeDefs = importSchema(path.join(__dirname, '..', '..', 'graphql', 'schema.graphql'));
+const schema = loadSchemaSync(path.join(__dirname, '..', '..', 'graphql', 'schema.graphql'), {
+    loaders: [new GraphQLFileLoader()],
+});
 
 function formatError(error: GraphQLError): GraphQLError {
     logGraphQlError(error);
     return error;
 }
 
-const server = new ApolloServer({
-    typeDefs,
+const schemaWithResolvers = addResolversToSchema({
+    schema,
     resolvers,
+});
+
+const server = new ApolloServer({
+    schema: schemaWithResolvers,
     formatError,
     // Enable interactive playground in production
     playground: true,
